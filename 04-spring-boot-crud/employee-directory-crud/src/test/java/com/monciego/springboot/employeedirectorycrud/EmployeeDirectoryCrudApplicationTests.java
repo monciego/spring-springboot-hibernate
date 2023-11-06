cc3b82feae8753d9d0583e70d3da56cc33670898
Jericho <jerichobantiquete@gmail.com>
2023-11-07 02:27:30 +0800

    chore: initialize project

---
+++ 04-spring-boot-crud/employee-directory-crud/src/test/java/com/monciego/springboot/employeedirectorycrud/EmployeeDirectoryCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monciego.springboot.employeedirectorycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirectoryCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
